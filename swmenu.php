0df512d9fcc824d1620f75e915e975b3e28a312b
swmenu <swmenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-12 04:09:57 +0000

    First Version upload
    git-svn-id: https://plugins.svn.wordpress.org/swmenu/trunk@819414 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ swmenu.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Plugin Name: swmenu
+ * Description: swMenu widget to display a menu created by swmenu.com
+ * Author: Sean White
+ * Version: 1.1
+ * Author URI: http://www.swmenu.com
+ */
+defined('ABSPATH') or die("Cannot access pages directly.");
+
+defined("DS") or define("DS", DIRECTORY_SEPARATOR);
+//error_reporting(E_ALL);
+
+class sw_swmenu {
+
+    function sw_swmenu() {
+        if (!is_admin()) {
+            // Header styles
+            add_action('wp_head', array('sw_swmenu', 'header'));
+          wp_enqueue_script('jquery');
+
+        }
+        
+    }
+
+    function header() {
+       $swmenupro=sw_swmenu::get_style();
+       echo "\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"" . sw_swmenu::get_plugin_directory() . "/css/menu.css\" media=\"screen\" />\n";
+       
+       if(@$swmenupro['responsive']==('flat'||'accordion')){
+         echo "<link type='text/css' href='" . sw_swmenu::get_plugin_directory() . "/css/menu_responsive.css' rel='stylesheet' />\n";
+       }
+       if(@$swmenupro['pie_hack']){
+         echo "<!--[if lt IE 9]>\n<link type='text/css' href='" . sw_swmenu::get_plugin_directory() . "/css/menu_pie.css' rel='stylesheet' />\n<![endif]-->\n";
+       }
+    }
+
+    function get_plugin_directory() {
+       return plugin_dir_url( __FILE__ );
+    }
+    
+    function get_style(){
+        $swmenupro=array();
+        $handle = fopen(sw_swmenu::get_plugin_directory() . '/vars.txt', 'r');
+        $import = fread($handle, 1000000);
+        fclose($handle);
+        eval('$swmenupro = ' . $import . ';');
+        return $swmenupro;
+        
+        }
+
+}
+
+;
+
+// Include the widget
+include_once(dirname( __FILE__ ) . '/swmenu_widget.php');
+
+// Initialize the plugin.
+$swmenu = new sw_swmenu();
+
+// Register the widget
+add_action('widgets_init', create_function('', 'return register_widget("sw_swmenu_widget");'));
+?>
